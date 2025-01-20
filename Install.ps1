5b4fc8057e8bc6f5ee0fe38d3cb60657d85766e5
Dmitry Nefedov <nefedovds@ya.ru>
2025-01-20 23:26:52 +0300

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,9 +1,11 @@
-# Install
-if (Test-Path $PSScriptRoot\Office\Data\*\stream.x64.x-none.dat)
+#Requires -RunAsAdministrator
+#Requires -Version 5.1
+
+if (Test-Path -Path $PSScriptRoot\Office\Data\*\stream.x64.x-none.dat)
 {
 	Start-Process -FilePath "$PSScriptRoot\setup.exe" -ArgumentList "/configure `"$PSScriptRoot\Config.xml`"" -Wait
 }
 else
 {
-	Write-Verbose -Message "There aren't neccessary Office files to install" -Verbose
+	Write-Verbose -Message "There aren't neccessary Office files to install. Please do not move Install.ps1 from where all files are." -Verbose
 }
